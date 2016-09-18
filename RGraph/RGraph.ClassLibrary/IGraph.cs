c9f731054f428b18ea93b7e91125ac8b6f48d366
Mayur Deore <msd7.1993@hotmail.com>
2016-09-18 13:01:41 +0530

    Class for Pie Chart added

---
+++ RGraph/RGraph.ClassLibrary/IGraph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGraph.ClassLibrary
+{
+    /// <summary>
+    /// Graph Interface
+    /// </summary>
+    public interface IGraph
+    {
+        /// <summary>
+        /// Gets data for Chart.
+        /// </summary>
+        /// <param name="ds">Dataset</param>
+        /// <returns>List of strings 
+
+        ///  </returns>
+        List<string> getGraphData(DataSet ds);
+    }
+}
