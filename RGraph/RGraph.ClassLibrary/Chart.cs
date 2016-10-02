d3335dd00d4d6c3ede74d26b60296ab2946adf58
Mayur Deore <msd7.1993@hotmail.com>
2016-10-02 18:39:46 +0530

    Confliction Resolved

---
+++ RGraph/RGraph.ClassLibrary/Chart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGraph
+{
+    public class Chart
+    {
+        public string Data { get; set; }
+
+        public string Label { get; set; }
+
+
+        public string Color { get; set; }
+    }
+}
